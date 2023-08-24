187a01c1a9d8a900e5df7a14209c8fca5de3e3ca
Smritikana Banerjee <74864386+smritikana31@users.noreply.github.com>
2023-08-24 23:04:02 +0530

    Create productData.js

---
+++ src/productData.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    title: "Boat Wireless Headphone",
+    price: 2400,
+    img: "https://res.cloudinary.com/doccscily/image/upload/v1692880524/photo-1505740420928-5e560c06d30e_sphndc.jpg",
+    quantity: 1,
+  },
+  {
+    id: 2,
+    title: "Samsung Headset",
+    price: 15000,
+    img: "https://res.cloudinary.com/doccscily/image/upload/v1692880839/download_czjdxu.jpg",
+    quantity: 1,
+  },
+  {
+    id: 3,
+    title: "Xiaomi Wired Headset",
+    price: 1800,
+    img: "https://res.cloudinary.com/doccscily/image/upload/v1692881112/top-view-phone-headphone-wood-background_1150-6887_ku66cu.jpg",
+    quantity: 1,
+  },
+  {
+    id: 4,
+    title: "Nokia Headset",
+    price: 2500,
+    img: "https://res.cloudinary.com/doccscily/image/upload/v1692880482/2-JBL-Tune-510BT-Wireless-On-Ear-Headphones-1024x640_yi1xvm.webp",
+    quantity: 1,
+  },
+  {
+    id: 5,
+    title: "Realme Wireless Headphone",
+    price: 5000,
+    img: "https://res.cloudinary.com/doccscily/image/upload/v1692880958/sony-mdr-zx110-ap-wired-headphone-832X558-1598367404_cfnvhe.webp",
+    quantity: 1,
+  },
+  {
+    id: 6,
+    title: "JBL Headset",
+    price: 50000.0,
+    img: "https://res.cloudinary.com/doccscily/image/upload/v1692881706/jobFeP6hgzFgHSiHFpnsUZ_ltej9r.jpg",
+    quantity: 1,
+  },
+];
